31e347a15ea0a607a0d5aa35e6f0ec7d81dcd9b3
久保祐輔 <bokukuboyu@icloud.com>
2022-05-10 16:33:02 +0900

    forth-commit

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   const navsm = document.getElementById("header-nav-sm");
   navsm.classList.toggle("in");
-  const logo = documemt.getElementById("header-logo");
-  logo.classList.toggle("logo-opacity");
+  
+  
   
   
 });
